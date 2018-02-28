b1f910154578e90a162328043117a9bf5783fb67
Phil Hack <phil@philhack.com>
2018-02-28 13:01:02 +0000

    ran bin/cucumber and pasted the contents into this .rb file

---
+++ features/step_definitions/achievements_steps.rb
@@ -0,0 +1,15 @@
+Given("I am a guest user") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("there is a public achievement") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I go to the achievement's page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I must see achievement's content") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
