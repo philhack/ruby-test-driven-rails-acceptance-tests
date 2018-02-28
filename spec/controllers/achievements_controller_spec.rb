89465e12352f83d2ae1f4133f5c3381c9bb595f0
Phil Hack <phil@philhack.com>
2018-02-28 19:21:34 +0000

    added invalid data controller test

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -27,15 +27,27 @@ describe AchievementsController do
   end
 
   describe "POST create" do
-    it "redirects to achievements#show" do
-      post :create, params: { achievement: FactoryGirl.attributes_for(:public_achievement)}
-      expect(response).to redirect_to(achievement_path(assigns[:achievement]))
+    context "valid data" do
+      it "redirects to achievements#show" do
+        post :create, params: { achievement: FactoryGirl.attributes_for(:public_achievement)}
+        expect(response).to redirect_to(achievement_path(assigns[:achievement]))
+      end
+
+      it "creates new achievement in database" do
+        expect {
+          post :create, params: { achievement: FactoryGirl.attributes_for(:public_achievement)}
+        }.to change(Achievement, :count).by(1)
+      end
     end
 
-    it "creates new achievement in database" do
-      expect {
-        post :create, params: { achievement: FactoryGirl.attributes_for(:public_achievement)}
-      }.to change(Achievement, :count).by(1)
+    context "invalid data" do
+      it "renders :new template" do
+        post :create, params: { achievement: FactoryGirl.attributes_for(:public_achievement, title: '')}
+        expect(response).to render_template(:new)
+
+      end
+      it "doesn't create a new achievement in the database"
     end
+
   end
 end
