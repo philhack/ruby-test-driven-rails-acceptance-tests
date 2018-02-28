5f5e9eb5b47b4f3af43ca38fa6465dd7c44fdcc8
Phil Hack <phil@philhack.com>
2018-02-28 19:45:40 +0000

    refactored tests to move duplicate data setup into context

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -13,7 +13,7 @@ describe AchievementsController do
   end
 
   describe "GET show" do
-    let(:achievement){ FactoryGirl.create(:public_achievement)}
+    let(:achievement){ FactoryGirl.create(:public_achievement) }
 
     it "renders :show template" do
       get :show, params: { id: achievement }
@@ -27,31 +27,34 @@ describe AchievementsController do
   end
 
   describe "POST create" do
+    let(:valid_data){ FactoryGirl.attributes_for(:public_achievement) }
+
     context "valid data" do
       it "redirects to achievements#show" do
-        post :create, params: { achievement: FactoryGirl.attributes_for(:public_achievement)}
+        post :create, params: { achievement: valid_data}
         expect(response).to redirect_to(achievement_path(assigns[:achievement]))
       end
 
       it "creates new achievement in database" do
         expect {
-          post :create, params: { achievement: FactoryGirl.attributes_for(:public_achievement)}
+          post :create, params: { achievement: valid_data}
         }.to change(Achievement, :count).by(1)
       end
     end
 
     context "invalid data" do
+      let(:invalid_data){ FactoryGirl.attributes_for(:public_achievement, title: '') }
+
       it "renders :new template" do
-        post :create, params: { achievement: FactoryGirl.attributes_for(:public_achievement, title: '')}
+        post :create, params: { achievement: invalid_data}
         expect(response).to render_template(:new)
 
       end
       it "doesn't create a new achievement in the database" do
         expect {
-          post :create, params: { achievement: FactoryGirl.attributes_for(:public_achievement, title: '')}
+          post :create, params: { achievement: invalid_data}
         }.not_to change(Achievement, :count)
       end
     end
-
   end
 end
