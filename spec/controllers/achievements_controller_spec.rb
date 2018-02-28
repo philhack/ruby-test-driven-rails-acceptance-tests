5a9d6295b033e14930933ef0d4dee9fe853d9a07
Phil Hack <phil@philhack.com>
2018-02-28 19:46:46 +0000

    fixed styles to adhere to rubocop

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -1,59 +1,59 @@
 require 'rails_helper'
 
 describe AchievementsController do
-  describe "GET new" do
-    it "renders :new template" do
+  describe 'GET new' do
+    it 'renders :new template' do
       get :new
       expect(response).to render_template(:new)
     end
-    it "assigns new Achievement to @acheivement" do
+    it 'assigns new Achievement to @achievement' do
       get :new
       expect(assigns(:achievement)).to be_a_new(Achievement)
     end
   end
 
-  describe "GET show" do
-    let(:achievement){ FactoryGirl.create(:public_achievement) }
+  describe 'GET show' do
+    let(:achievement) { FactoryGirl.create(:public_achievement) }
 
-    it "renders :show template" do
+    it 'renders :show template' do
       get :show, params: { id: achievement }
       expect(response).to render_template(:show)
     end
 
-    it "assigns requested achievement to @achievement" do
+    it 'assigns requested achievement to @achievement' do
       get :show, params: { id: achievement }
       expect(assigns(:achievement)).to eq(achievement)
     end
   end
 
-  describe "POST create" do
-    let(:valid_data){ FactoryGirl.attributes_for(:public_achievement) }
+  describe 'POST create' do
+    let(:valid_data) { FactoryGirl.attributes_for(:public_achievement) }
 
-    context "valid data" do
-      it "redirects to achievements#show" do
+    context 'valid data' do
+      it 'redirects to achievements#show' do
         post :create, params: { achievement: valid_data}
         expect(response).to redirect_to(achievement_path(assigns[:achievement]))
       end
 
-      it "creates new achievement in database" do
-        expect {
+      it 'creates new achievement in database' do
+        expect do
           post :create, params: { achievement: valid_data}
-        }.to change(Achievement, :count).by(1)
+        end.to change(Achievement, :count).by(1)
       end
     end
 
-    context "invalid data" do
-      let(:invalid_data){ FactoryGirl.attributes_for(:public_achievement, title: '') }
+    context 'invalid data' do
+      let(:invalid_data) { FactoryGirl.attributes_for(:public_achievement, title: '') }
 
-      it "renders :new template" do
+      it 'renders :new template' do
         post :create, params: { achievement: invalid_data}
         expect(response).to render_template(:new)
 
       end
       it "doesn't create a new achievement in the database" do
-        expect {
+        expect do
           post :create, params: { achievement: invalid_data}
-        }.not_to change(Achievement, :count)
+        end.not_to change(Achievement, :count)
       end
     end
   end
