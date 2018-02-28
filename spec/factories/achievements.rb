e021efd959acef657714752aeb3b5bd4712900d6
Phil Hack <phil@philhack.com>
2018-02-28 12:24:28 +0000

    added FactoryGirl factories to tests

--- spec/factories/achievements.rb
+++ spec/factories/achievements.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :achievement do
-    title "MyString"
-    description "MyText"
-    privacy 1
-    featured false
-    cover_image "MyString"
+  title "Title"
+  description "description"
+  privacy Achievement.privacies[:private_access]
+  featured false
+  cover_image "some_file.png"
   end
 end
--- spec/features/achievement_spec.rb
+++ spec/features/achievement_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 feature 'achievement page' do
   scenario 'achievement public page' do
-    achievement = Achievement.create(title: 'Just did it')
+    achievement = FactoryGirl.create(:achievement, title: 'Just did it')
     visit("/achievements/#{achievement.id}")
 
     expect(page).to have_content('Just did it')
+  end
+
+  scenario 'render markdown description' do
+    achievement = FactoryGirl.create(:achievement, description: 'That *was* hard')
+    visit("/achievements/#{achievement.id}")
+
+    expect(page).to have_content('<i>was</i>>')
   end
 end
