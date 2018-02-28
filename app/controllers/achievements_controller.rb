9278e16b72f89019430a5f27fd1f3e6256ee7bca
Phil Hack <phil@philhack.com>
2018-02-28 16:44:42 +0000

    made achievement test pass.

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -5,8 +5,8 @@ class AchievementsController < ApplicationController
 
   def create
     @achievement = Achievement.new(achievement_params)
-    if(@achievement.save)
-      redirect_to root_url, notice: 'Achievement has been created'
+    if @achievement.save
+      redirect_to achievement_url(@achievement), notice: 'Achievement has been created'
     else
       render :new
     end
@@ -18,6 +18,7 @@ class AchievementsController < ApplicationController
 
 
   private
+
   def achievement_params
     params.require(:achievement).permit(:title, :description, :privacy, :cover_image, :featured )
   end
