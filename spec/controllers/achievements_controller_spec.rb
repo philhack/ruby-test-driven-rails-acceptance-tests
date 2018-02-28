5ef1e557ca3335ab833e401d42fd43d53fc59d51
Phil Hack <phil@philhack.com>
2018-02-28 14:19:17 +0000

    made first test pass

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 describe AchievementsController do
-
+  describe "GET new" do
+    it "renders :new template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+    it "assigns new Achievement to @acheivement"
+  end
 end
